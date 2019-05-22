cdf776d3ea1e9442e33205db67e0adecc731ba42
Blue Circus <wictornisa@hotmail.com>
2019-05-22 13:04:31 +0200

    save

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const mainNav = document.getElementById('mainNav')
+const topOfNav = mainNav.offsetTop;
+
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
@@ -10,10 +13,20 @@ const navSlide = () => {
       if(link.style.animation){
         link.style.animation = ''
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.9}s`
       }
     });
+    
   });
 }
 
+function stickyNav(){
+  if(window.scrollY >= topOfNav){
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+  } 
+}
+
+window.addEventListener('scroll', stickyNav);
 navSlide();
